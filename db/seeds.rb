47b5254e9d464572e5b0f63422c3e43e0ac68954
itssomething <vdmanh107@gmail.com>
2018-10-16 23:59:45 +0700

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,22 @@ end
 
 question_banks = QuestionBank.all
 
-question_banks.each do |question_bank|
-  50.times do
+question_banks.each do |question_bank, m|
+  puts "bank no #{m}"
+  30.times do |n|
     Question.create!([
       {content: Faker::Lorem.sentence, question_bank_id: question_bank.id}
     ])
+    puts "question no #{n}"
   end
 end
 
 questions = Question.all
 
-questions.each do |question|
+puts "-----------BEGIN CREATING ANSWERS--------"
+
+questions.each do |question, n|
+  puts "question no #{n}"
   4.times do
     Answer.create!([
       {content: Faker::Lorem.sentence, question_id: question.id}
